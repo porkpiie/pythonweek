c146147a38f32d3d919233c32eb1b6b21b0bb915
porkpiie <adtready@gmail.com>
2019-05-24 10:34:56 +0100

    results2

---
+++ results2.py
@@ -0,0 +1,12 @@
+Physics=80
+Chemistry=100
+Math=100
+Total=Physics+Chemistry+Math
+per=Total/450*100
+print("Physics:", Physics)
+print("Chemistry:", Chemistry)
+print("Math:", Math)
+print("###########################")
+print("Total Marks:", Total)
+print("Percentage:", per)
+print("###########################")
