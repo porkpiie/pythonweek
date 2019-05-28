4acbc5dc9039ad6aa1eb1bcfc81e8985b5386141
porkpiie <adtready@gmail.com>
2019-05-28 11:56:46 +0100

    nested loops, len command

---
+++ len1.py
@@ -0,0 +1,7 @@
+msg=input ("Enter any message")
+
+i=0
+
+while i<len(msg):
+    print(msg[i])
+    i=i+1
---
+++ nestedloop2.py
@@ -0,0 +1,9 @@
+a=1
+
+while a<=10:
+    b=1
+    while b<=a:
+        print(a)
+        b=b+1
+    a=a+1
+    
